de9a75a9e7876972b3158a2d49c9ab487fe00cab
Marcus Wehbi <marcus.wehbi@stu.ocsb.ca>
2022-09-13 23:27:35 +0000

    UpdatedTheHelloWorldFile

---
+++ hello_world_updated.py
@@ -0,0 +1,3 @@
+print ("Hello Ms Raffin, this is a message from Marcus Wehbi.")
+print ("I want you to know that I appreciate the hard work you put into the class.")
+print ("I am really enjoying programming in this class.")
